34345d9f3a198a84df9adeab023337977d5f0bc7
Dakota Cookenmaster <dakotacookenmaster@southern.edu>
2023-11-29 12:58:09 -0500

    Updated Week 12 code

--- Week 12/client/src/App.jsx
+++ Week 12/client/src/App.jsx
@@ -1,33 +1,62 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [color, setColor] = useState("#ccc")
+  const [color, setColor] = useState("#ccc");
+  const [name, setName] = useState("")
+  const [names, setNames] = useState([])
 
-  useEffect(() => {
-    const getData = async () => {
-      const rawData = await fetch("http://localhost:3000")
-      const jsonData = await rawData.json()
-      setColor(jsonData.color)
-    }
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const rawData = await fetch("http://localhost:3000");
+  //     const jsonData = await rawData.json();
+  //     setColor(jsonData.color);
+  //   };
 
-    const dataInterval = setInterval(getData, 500)
+  //   const dataInterval = setInterval(getData, 500);
 
-    return () => {
-      clearInterval(dataInterval)
-    }
-  }, [])
+  //   return () => {
+  //     clearInterval(dataInterval);
+  //   };
+  // }, []);
+
+  const submitName = async () => {
+    const rawData = await fetch("http://localhost:3000/names", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name })
+    })
+
+    const jsonData = await rawData.json()
+    setNames(jsonData)
+  }
 
   return (
-    <div style={{
-      width: "100px",
-      height: "100px",
-      border: "2px solid black",
-      backgroundColor: color,
-    }}></div>
-  )
+    <>
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          border: "2px solid black",
+          backgroundColor: color,
+        }}
+      ></div>
+      <section>
+        <input value={name} onChange={(event) => setName(event.target.value)}/>
+        <button onClick={submitName}>Submit</button>
+      </section>
+      <section>
+        { names.map((name, index) => {
+          return <p key={index}>{ name }</p>
+        })}
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
--- Week 12/index.js
+++ Week 12/index.js
@@ -1,15 +1,30 @@
 import express from "express"
 import randomColor from "randomcolor"
 import cors from "cors"
+import bodyparser from "body-parser"
+
 const app = express()
 const port = 3000
 
 app.use(cors())
+app.use(bodyparser.json())
+
+const names = []
 
 app.get("/", (request, response) => {
     return response.json({
         color: randomColor()
     })
+})
+
+app.post("/names", (request, response) => {
+    const { name } = request.body
+    names.push(name)
+    return response.json(names)
+})
+
+app.get("/names", (request, response) => {
+    return response.json(names)
 })
 
 app.listen(port, () => {
